95049fe6321096ccb01f7891d33f4e5041bc3ffd
dombap <aparnadomb3933@gmail.com>
2022-03-31 16:26:28 +0530

    backend changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // server.js
 var express = require('express');
 var bodyParser = require('body-parser');
+// const nodemailer = require('nodemailer');
 const cors= require('cors');
 var app = express();
 app.use(cors());
@@ -16,7 +17,15 @@ app.use(bodyParser.json({
   type: 'application/vnd.api+json'
 })); // Parse application/vnd.api+json as json
 require('./routes')(app, {});
-
+// const transporter = nodemailer.createTransport({
+//   host: 'smtp.gmail.com',
+//   port: 587,
+//   auth: {
+//     user: 'aparnadomb3933@gmail.com',
+//     pass: 'Aparna@123',
+//   },
+// });
+// transporter.verify().then(console.log).catch(console.error);
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
